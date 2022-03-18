fb857b65b2d087b59d83a8e10d6cd0a336c8dbaa
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-03-17 22:16:01 -0400

    Initial commit

---
+++ src/main/java/healpoolremake/bwhealpool/BWhealpool.java
@@ -0,0 +1,60 @@
+package healpoolremake.bwhealpool;
+
+import com.andrei1058.bedwars.api.BedWars;
+import com.andrei1058.bedwars.api.arena.IArena;
+import com.andrei1058.bedwars.api.arena.team.ITeam;
+import com.andrei1058.bedwars.api.events.gameplay.GameEndEvent;
+import com.andrei1058.bedwars.api.events.server.ArenaDisableEvent;
+import com.andrei1058.bedwars.api.events.upgrades.UpgradeBuyEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BWhealpool extends JavaPlugin implements Listener {
+
+    public static BWhealpool plugin;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        plugin = this;
+
+        //Check for Bedwars
+        if (!Bukkit.getPluginManager().isPluginEnabled("Bedwars1058")){
+            this.getLogger().info("You must install Bedwars1058");
+            this.setEnabled(false);
+            return;
+        }
+        else
+        {
+            this.getLogger().info("Detected Bedwars1058!");
+        }
+
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onTeamUpgrade(UpgradeBuyEvent e){
+        if (e.getTeamUpgrade().getName().contains("heal")){
+            IArena a = e.getArena();
+            if (a == null) return;
+            ITeam bwt = a.getTeam(e.getPlayer());
+            if (bwt == null) return;
+            if (!HealPoolTask.exists(a, bwt)){
+                new HealPoolTask(bwt);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDisable(ArenaDisableEvent e){HealPoolTask.removeForArena(e.getArenaName());}
+
+    @EventHandler
+    public void onEnd(GameEndEvent e) {HealPoolTask.removeForArena(e.getArena());}
+
+    @EventHandler
+    public void teamDead() {}
+}
---
+++ src/main/java/healpoolremake/bwhealpool/HealPoolTask.java
@@ -0,0 +1,87 @@
+package healpoolremake.bwhealpool;
+
+import com.andrei1058.bedwars.api.arena.IArena;
+import com.andrei1058.bedwars.api.arena.team.ITeam;
+import com.andrei1058.bedwars.api.configuration.ConfigPath;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HealPoolTask extends BukkitRunnable {
+
+    private ITeam bwt;
+    private int maxX, minX, maxY, minY, maxZ, minZ;
+    private IArena arena;
+    private Random r = new Random();
+    private Location l;
+
+    private static List<HealPoolTask> healPoolTaskList = new ArrayList<>();
+
+    public HealPoolTask(ITeam bwt){
+        this.bwt = bwt;
+        int radius = bwt.getArena().getConfig().getInt(ConfigPath.ARENA_ISLAND_RADIUS);
+        this.maxX = Math.max(bwt.getSpawn().clone().add(radius, 0 , 0).getBlockX(),bwt.getSpawn().clone().subtract(radius, 0, 0).getBlockX());
+        this.minX = Math.min(bwt.getSpawn().clone().add(radius, 0, 0).getBlockX(), bwt.getSpawn().clone().subtract(radius, 0, 0).getBlockX());
+        this.maxY = Math.max(bwt.getSpawn().clone().add(0, radius, 0).getBlockY(), bwt.getSpawn().clone().subtract(0, radius, 0).getBlockY());
+        this.minY = Math.min(bwt.getSpawn().clone().add(0, radius, 0).getBlockY(), bwt.getSpawn().clone().subtract(0, radius, 0).getBlockY());
+        this.maxZ = Math.max(bwt.getSpawn().clone().add(0, 0, radius).getBlockZ(), bwt.getSpawn().clone().subtract(0, 0, radius).getBlockZ());
+        this.minZ = Math.min(bwt.getSpawn().clone().add(0, 0, radius).getBlockZ(), bwt.getSpawn().clone().subtract(0, 0, radius).getBlockZ());
+        this.arena = bwt.getArena();
+        this.runTaskTimer(BWhealpool.plugin, 0, 80L);
+        healPoolTaskList.add(this);
+    }
+
+    @Override
+    public void run(){
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                for (int z = minZ; z < maxZ; z++) {
+                    l = new Location(bwt.getSpawn().getWorld(), x, y, z);
+                    if (l.getBlock().getType() != Material.AIR) continue;
+                    int chance = r.nextInt(5);
+                    if (chance == 0) {
+                        for (Player p : bwt.getMembers()) {
+                            p.spawnParticle(Particle.VILLAGER_HAPPY, l, 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean exists(IArena arena, ITeam bwt){
+        for (HealPoolTask hpt : new ArrayList<>(healPoolTaskList)) {
+            if (hpt.getArena()  == arena && hpt.getBwt() == bwt) return true;
+        }
+        return false;
+    }
+
+    public static void removeForArena(IArena a){
+        for (HealPoolTask hpt: new ArrayList<>(healPoolTaskList)) {
+            if (hpt.getArena().getWorldName().equals(a)){
+                healPoolTaskList.remove(hpt);
+                hpt.cancel();
+            }
+        }
+    }
+
+    public  static void removeForArena(String a){
+        for (HealPoolTask hpt: new ArrayList<>(healPoolTaskList)) {
+            if (hpt.getArena().getWorldName().equals(a)){
+                healPoolTaskList.remove(hpt);
+                hpt.cancel();
+            }
+        }
+    }
+
+    public  ITeam getBwt() {return  bwt;}
+
+    public IArena getArena() {return arena;}
+
+}
